1a9f78cd7dd5278f8cbe6173fb092798ebc85980
gcstokesanu@gmail.com^C <gcstokesanu@gmail.com>
2025-09-25 10:15:49 -0500

    update PS4 solutions

---
+++ ProblemSets/PS4-mixture/PS4_Gao_script.jl
@@ -0,0 +1,10 @@
+using Random, LinearAlgebra, Statistics, Optim, DataFrames, CSV, HTTP, GLM, FreqTables, Distributions, JuliaFormatter
+
+cd(@__DIR__)
+
+format("PS4_Gao_source.jl")
+
+include("PS4_Gao_source.jl")
+
+
+allwrap()
