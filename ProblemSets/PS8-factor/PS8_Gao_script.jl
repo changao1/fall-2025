5aa75ac8e4f3553f25252fd3bda3cc1adb4cdb40
Chang Gao <gcstokesanu@gmail.com>
2025-10-30 10:11:27 -0500

    update PS8

---
+++ ProblemSets/PS8-factor/PS8_Gao_script.jl
@@ -0,0 +1,11 @@
+using Random, LinearAlgebra, Statistics, Distributions
+using Optim, DataFrames, DataFramesMeta, CSV, HTTP, GLM
+using MultivariateStats, FreqTables, ForwardDiff
+
+
+cd(@__DIR__)
+
+
+include("PS8_Gao_source.jl")
+
+main()
---
+++ ProblemSets/PS8-factor/PS8_Gao_tests.jl
@@ -0,0 +1,8 @@
+using Test
+using Random, LinearAlgebra, Statistics, Distributions
+using Optim, DataFrames, DataFramesMeta, CSV, HTTP, GLM
+using MultivariateStats, FreqTables, ForwardDiff
+
+include("PS8_Gao_source.jl")
+
+# Define tests and run them
